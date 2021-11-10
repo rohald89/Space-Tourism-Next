14b39591318d3b2b4ad86c3239edcf5dcf601534
Rohald van Merode <rohald_89@live.nl>
2021-11-10 21:13:06 +0100

    improved mobile nav to close upon clicking a link

--- components/Nav.js
+++ components/Nav.js
@@ -1,34 +1,56 @@
-import { useRouter } from "next/router";
-import { useState } from "react";
-import Link from "next/link";
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+import Link from 'next/link';
 
 const Nav = () => {
-    const {pathname} = useRouter();
+    const { pathname } = useRouter();
     const currentPath = pathname === '/' ? 'home' : pathname.slice(1);
-    const [ isOpen, setIsOpen ] = useState(false);
+    const [isOpen, setIsOpen] = useState(false);
 
     const toggleMenu = (e) => {
-        setIsOpen(prev => !prev);
-    }
+        setIsOpen((prev) => !prev);
+    };
 
-    const pages = ['home', 'destination', 'crew', 'technology']
+    const pages = ['home', 'destination', 'crew', 'technology'];
 
     return (
         <>
-            <button onClick={toggleMenu} className="mobile-nav-toggle" aria-controls="primary-navigation" aria-expanded={isOpen}><span className="sr-only">Menu</span></button>
+            <button
+                onClick={toggleMenu}
+                className="mobile-nav-toggle"
+                aria-controls="primary-navigation"
+                aria-expanded={isOpen}
+            >
+                <span className="sr-only">Menu</span>
+            </button>
             <nav>
-                <ul id="primary-navigation" data-visible={isOpen} className="primary-navigation underline-indicators flex">
-                    { pages.map((page, i) => (
-                        <li key={page} className={currentPath === page ? 'active' : ''}>
-                            <Link href={`${page === 'home' ? "/" : `/${page}`}`}>
-                                <a className="ff-sans-cond uppercase text-white letter-spacing-2"><span aria-hidden="true">0{i}</span>{page}</a>
+                <ul
+                    id="primary-navigation"
+                    data-visible={isOpen}
+                    className="primary-navigation underline-indicators flex"
+                >
+                    {pages.map((page, i) => (
+                        <li
+                            key={page}
+                            className={currentPath === page ? 'active' : ''}
+                        >
+                            <Link
+                                href={`${page === 'home' ? '/' : `/${page}`}`}
+                            >
+                                <a
+                                    onClick={toggleMenu}
+                                    className="ff-sans-cond uppercase text-white letter-spacing-2"
+                                >
+                                    <span aria-hidden="true">0{i}</span>
+                                    {page}
+                                </a>
                             </Link>
                         </li>
                     ))}
                 </ul>
             </nav>
         </>
-    )
-}
+    );
+};
 
-export default Nav
+export default Nav;
