2b10245175fed00eb4847ec33ff1f4332e258d3c
Rohald van Merode <rohald_89@live.nl>
2021-11-15 20:09:41 +0100

    fixed escaping issue

--- data.js
+++ data.js
@@ -18,7 +18,7 @@ const data = {
                 webp: '/../public/destination/image-mars.webp',
             },
             description:
-                'Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!',
+                "Don't forget to pack your hiking boots. You'll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!",
             distance: '225 mil. km',
             travel: '9 months',
         },
--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,32 @@
 import Link from 'next/link';
 
 export default function Home() {
-
-  return (
-    <>
-    <main id="main" className="grid-container grid-container--home">
-        <div>
-          <h1 className="text-accent fs-500 ff-sans-cond uppercase letter-spacing-1">So, you want to travel to
-          <span className="fs-900 ff-serif text-white d-block">Space</span></h1>
-          <p>Let's face it; if you want to go to space, you might as well genuinely go to 
-          outer space and not hover kind of on the edge of it. Well sit back, and relax 
-          because we'll give you a truly out of this world experience! </p>
-        </div>
-        <div>
-          <Link href="destination">
-            <a className="large-button uppercase ff-serif text-dark bg-white">Explore</a>
-          </Link>
-        </div>
-    </main>
-    </>
-  )
+    return (
+        <>
+            <main id="main" className="grid-container grid-container--home">
+                <div>
+                    <h1 className="text-accent fs-500 ff-sans-cond uppercase letter-spacing-1">
+                        So, you want to travel to
+                        <span className="fs-900 ff-serif text-white d-block">
+                            Space
+                        </span>
+                    </h1>
+                    <p>
+                        Let&apos;s face it; if you want to go to space, you
+                        might as well genuinely go to outer space and not hover
+                        kind of on the edge of it. Well sit back, and relax
+                        because we&apos;ll give you a truly out of this world
+                        experience!{' '}
+                    </p>
+                </div>
+                <div>
+                    <Link href="destination">
+                        <a className="large-button uppercase ff-serif text-dark bg-white">
+                            Explore
+                        </a>
+                    </Link>
+                </div>
+            </main>
+        </>
+    );
 }
